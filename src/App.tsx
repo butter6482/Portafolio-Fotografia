c05c5e690dd3b4f64b642f902dc555be59e4c798
Alejandro Butter <alejandrobutter316@gmail.com>
2026-08-13 21:29:24 -0400

    feat: add react-router-dom, /admin route, useSiteSettings in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Navbar } from './components/Navbar';
-import { Hero } from './components/Hero';
-import { About } from './components/About';
-import { Gallery } from './components/Gallery';
-import { InstagramSection } from './components/InstagramSection';
-import { Footer } from './components/Footer';
-export function App() {
-  return <div className="w-full min-h-screen bg-white text-gray-900 font-sans">
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { Hero } from './components/Hero'
+import { About } from './components/About'
+import { Gallery } from './components/Gallery'
+import { InstagramSection } from './components/InstagramSection'
+import { Footer } from './components/Footer'
+// @ts-ignore
+import { AdminApp } from './admin/AdminApp'
+import { useSiteSettings } from './hooks/useSiteSettings'
+
+function PublicSite() {
+  return (
+    <div className="w-full min-h-screen font-sans" style={{ background: 'var(--color-bg)', color: 'var(--color-text)' }}>
       <Navbar />
       <main>
         <Hero />
@@ -15,5 +21,18 @@ export function App() {
         <InstagramSection />
       </main>
       <Footer />
-    </div>;
+    </div>
+  )
+}
+
+export function App() {
+  useSiteSettings()
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PublicSite />} />
+        <Route path="/admin/*" element={<AdminApp />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
